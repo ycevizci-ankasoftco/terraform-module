478c117a9ffc8bf5ce7acf37e3b6e7c4a183baab
Yusuf Emre Cevizci <133774006+ycevizci-ankasoftco@users.noreply.github.com>
2024-05-03 10:55:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,8 @@
 provider "azurerm" {
   features {}
     skip_provider_registration = true
-    subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    tenant_id       = var.tenant_id
 }
 
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
 variable "location" {}
 
 # Kimlik bilgilerini .tfvars dosyasından yükleme
